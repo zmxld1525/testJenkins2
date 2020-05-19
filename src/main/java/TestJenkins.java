6239af1dc2beb0e76168667fd241407b449e89e8
zmxld1525 <y1141582347@qq.com>
2020-05-19 10:28:41 +0800

    Initial commit

---
+++ src/main/java/TestJenkins.java
@@ -0,0 +1,5 @@
+public class TestJenkins {
+    public static void main(String[] args) {
+        System.out.println("its a good day");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>test Jenkins</h2>
+</body>
+</html>
